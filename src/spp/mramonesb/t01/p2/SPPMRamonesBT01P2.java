4784391872224746883affed1caae37634398552
marielaramonesbalvoa <marielaramonesbalvoa@10.34.157.204>
2017-01-30 15:33:46 -0600

    superficie y perimetro de un rectangulo

---
+++ src/spp/mramonesb/t01/p2/SPPMRamonesBT01P2.java
@@ -0,0 +1,53 @@
+//Mariela Ramones Balvoa- A01411449- IMT
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp.mramonesb.t01.p2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author marielaramonesbalvoa
+ */
+public class SPPMRamonesBT01P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Calculador de superficie y perimetro");    
+    //Paso1. variables
+    double base, altu;
+    double oper;
+    base=solicitarDatos("base");
+    altu=solicitarDatos("altura");
+    oper = operacion(base,altu);
+    }
+    // paso2. introducir datos
+    public static double solicitarDatos(String n){
+    //Paso 2. introducir datos
+    double datos;
+    Scanner kb= new Scanner (System.in);
+    
+    System.out.println("Introduce de la " + n + "de tu rectangulo");
+    datos=kb.nextDouble();
+    return datos;
+    }
+    public static int operacion (double base, double altu){    
+    //Paso 3. operaciones
+    int sup, per;
+    sup= (int) (base*altu);
+    System.out.println("La superficie es " + sup);
+    
+    per= (int) ((base*altu)/2);
+    System.out.println("El perimetro es " + per);
+    return sup;
+    }
+
+
+    }
+    
+
+
